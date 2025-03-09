c35b25274a2fcae968190f370f1984522bf4a4df
Valmir Murtezani <valmirmurtezani811@gmail.com>
2025-03-09 14:04:30 +0100

    Hello World

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="App-logo" alt="logo" />
+        <img src={viteLogo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+      </header>
+    </div>
+  )
+}
